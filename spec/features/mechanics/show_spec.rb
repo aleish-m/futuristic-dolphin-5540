a85c3557e397ffee541258bb61537e84ddae3fcd
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-12 11:35:47 -0600

    confirm mechanic show page has new ride added to page

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -25,8 +25,11 @@ RSpec.describe "Mechanic Show Page" do
 
         expect(page).to have_content("Name: Bob")
         expect(page).to have_content("Years of Experience: 8")
-        expect(page).to have_content("The Hurler")
-        expect(page).to have_content("The Scrambler")
+
+        within("#rides") do
+          expect(page).to have_content("The Hurler")
+          expect(page).to have_content("The Scrambler")
+        end
         
         expect(page).to_not have_content("Name: Jimmy")
         expect(page).to_not have_content("Years of Experience: 9")
@@ -36,8 +39,10 @@ RSpec.describe "Mechanic Show Page" do
       it 'I see a list of all mechanics names and their years of experience' do
         visit "/mechanics/#{@bob.id}"
 
-        expect(page).to have_content("The Hurler")
-        expect(page).to have_content("The Scrambler")
+        within("#rides") do
+          expect(page).to have_content("The Hurler")
+          expect(page).to have_content("The Scrambler")
+        end
         
         expect(page).to_not have_content("Ferris Wheel")
       end
@@ -45,28 +50,46 @@ RSpec.describe "Mechanic Show Page" do
       it 'the rides are listed by thrill rating in descending order (most thrills first)' do
         visit "/mechanics/#{@bob.id}"
 
-        expect(page).to have_content("Rides being worked on: The Hurler and The Scrambler")
+        within("#rides") do
+          expect(page).to have_content("Rides being worked on: The Hurler and The Scrambler")
+        end
       end
 
       it 'I see a form to add a ride to their workload' do
         visit "/mechanics/#{@bob.id}"
 
-        expect(page).to have_content("Add Ride to Workload:")
+        within("#add-ride") do
+          expect(page).to have_content("Add Ride to Workload:")
         
-        page.has_field? "ride_id"
+          page.has_field? "ride_id"
+        end
       end
 
       it "I fill in that field with an id of an existing ride and hit submit, I’m taken back to that mechanic's show page" do
         visit "/mechanics/#{@bob.id}"
 
-        fill_in("ride_id", with: "#{@jaws.id}")
+        within("#add-ride") do
+          fill_in("ride_id", with: "#{@jaws.id}")
 
-        click_button('Submit')
+          click_button('Submit')
+        end
 
         expect(current_path).to eq("/mechanics/#{@bob.id}")
       end
 
-      it ""
+      it "I see the name of that newly added ride on this mechanics show page" do
+        visit "/mechanics/#{@bob.id}"
+
+        within("#add-ride") do
+          fill_in("ride_id", with: "#{@jaws.id}")
+
+          click_button('Submit')
+        end
+
+        within("#rides") do
+          expect(page).to have_content("Jaws")
+        end
+      end
     end
   end
 end
